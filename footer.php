4b0c048fc423a6805a6e3f538fff30ec3a5978d3
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-04-21 13:52:17 +0200

    initial commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Outline_Slim
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'outline_slim' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'outline_slim' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'outline_slim' ), 'outline_slim', '<a href="https://automattic.com/" rel="designer">Nikolaus Schlifke</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
